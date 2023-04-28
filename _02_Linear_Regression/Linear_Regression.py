cef01ebd740eb05560be6fc1737168d56572dfff
162645 <2051699331@qq.com>
2023-04-28 09:19:00 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,7 +9,12 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
+    #读取数据
+    X,y = read_data('D:/myfile/data/shenjingwangluo/linear-regression-162645/data/exp02/')
+    #weight = np.ones((6,1))
+    for i in range(100):#迭代次数100次
+        weight = np.matmul(np.linalg.inv(np.matmul(X.T,X)),np.matmul(X.T,y))
+    return weight @ data
     
 def lasso(data):
     pass
@@ -18,3 +23,9 @@ def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+def main():
+    data = np.array([1, 2, 3, 4, 5, 6])  # 输入数据
+    print(ridge(data))  # 调用main函数，打印一个输出值
+    print(lasso(data))
+
+#main()
